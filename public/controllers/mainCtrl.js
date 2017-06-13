c7852176a46a98e0000c7e8e2d1f6ad1ce3eea7e
Justin Johnson <justin.johnson@SB-FL-K4XCGTFM.local>
2017-06-13 16:55:06 -0400

    Added gruntfile.js to watch/uglify

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -1,55 +1,51 @@
-myApp.controller('mainCtrl', ['$rootScope', '$location', 'FileService', '$scope', function ($rootScope, $location, FileService, $scope) {
+myApp.controller('mainCtrl', ['$rootScope', '$location', 'FileService', '$scope', function($rootScope, $location, FileService, $scope) {
 
-  var app = this;
+    var app = this;
 
-  // using loadme to hide HTML until loadme is true
-  // this will prevent the angular {{ }} displaying during page loading
-  app.loadme = false;
-  app.loadme = true;
+    // using loadme to hide HTML until loadme is true
+    // this will prevent the angular {{ }} displaying during page loading
+    // app.loadme = false
+    app.loadme = true;
 
+    $scope.createDirectory = function() {
+        FileService.createDirectory($scope.uploadData.genericName).then(function(data) {
+            if (data) {
+                console.log(data);
+            } else {
+                console.log('error creating new directory');
+            }
+        });
+    };
 
-  $scope.createDirectory = function () {
-    FileService.createDirectory($scope.uploadData.genericName).then(function (data) {
-      if (data) {
-        console.log(data);
-      } else {
-        console.log('error creating new directory');
-      }
+    $scope.getAvailableDirectories = function() {
+        FileService.getAvailableDirectories('uploads').then(function(data) {
+            if (data) {
+                console.log(data);
+            } else {
+                console.log('error getting list of directories :(');
+            }
+        });
+    };
+
+    $scope.pillClasses = ['Sedative', 'Opiate', 'Amphetamine', 'Psychedellic', 'Synthetic', 'Barbiturate', 'Pain Reliever', 'Antibiotic', 'OTC'];
+    $scope.pillClass = $scope.pillClasses[0];
+
+    $scope.uploadData = {};
+    $scope.Submit = function() {
+        FileService.postFile($scope.uploadData).then(function(data) {
+            if (data) {
+                console.log(data);
+            } else {
+                console.log('error uploading file');
+            }
+        });
+    }
+
+    /*
+     * Invoked when route changes
+     */
+    $rootScope.$on('$routeChangeStart', function() {
+        console.log('route change', $location);
     });
-  };
 
-  $scope.getAvailableDirectories = function () {
-    FileService.getAvailableDirectories('uploads').then(function (data) {
-      if (data) {
-        console.log(data);
-      } else {
-        console.log('error getting list of directories :(');
-      }
-    });
-  };
-
-
-  $scope.pillClasses = ['Sedative', 'Opiate', 'Amphetamine', 'Psychedellic', 'Synthetic', 'Barbiturate', 'Pain Reliever', 'Antibiotic', 'OTC'];
-  $scope.pillClass = $scope.pillClasses[0];
-
-
-  $scope.uploadData = {};
-  $scope.Submit = function () {
-    FileService.postFile($scope.uploadData).then(function (data) {
-      if (data) {
-        console.log(data);
-      } else {
-        console.log('error uploading file');
-      }
-    });
-
-  }
-
-  /*
-   * Invoked when route changes
-   */
-  $rootScope.$on('$routeChangeStart', function () {
-    console.log('route change', $location);
-  });
-
-      }]);
+}]);
