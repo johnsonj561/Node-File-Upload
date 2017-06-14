a463f33682a63020ef0e830f955c7b1f79a22ad7
Justin Johnson <justin.johnson@SB-FL-K4XCGTFM.local>
2017-06-14 09:56:37 -0400

    Added cssmin to grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,46 @@
 module.exports = function(grunt) {
 
-    // project configuration
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'public/controllers/mainCtrl.js',
-                dest: 'public/controllers/min/mainCtrl.min.js'
-            }
-        },
-        watch: {
-            files: ['public/controllers/mainCtrl.js'],
-            tasks: ['uglify']
-        }
-    });
+  // project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // uglify: {
+    //   options: {
+    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //   },
+    //   build: {
+    //     src: 'public/controllers/mainCtrl.js',
+    //     dest: 'public/controllers/min/mainCtrl.min.js'
+    //   }
+    // },
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['watch']);
+    // minifies all css files found in public/assetts/css, doesn't include already minified files
+    cssmin: {
+      my_target: {
+        // combine: {       use this to combine multiple css files into 1 file
+        //   files: {
+        //     'css/style.css': ['css/style2.css', 'css/style3.css']
+        //   }
+        // }
+        files: [{
+          expand: true, // expand enables the options found below, false by default
+          cwd: 'public/assetts/css/',
+          src: ['*.css', '!*.min.css'],
+          dest: 'public/assetts/css/',
+          ext: '.min.css'
+        }]
+      }
+    }
+
+
+    // watch: {
+    //   files: ['public/controllers/mainCtrl.js'],
+    //   tasks: ['uglify']
+    // }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['cssmin']);
 }
